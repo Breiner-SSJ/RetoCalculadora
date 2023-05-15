08ab2cff3bc12c82fc2a2eae7ebff9b5e824e21f
Breiner Moya <breinermoya2512@gmail.com>
2023-05-15 13:58:42 -0500

    Creación de menu

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido, Elige que operacion desea hacer");
+            Console.WriteLine("1 = Suma");
+            Console.WriteLine("2 = Resta");
+            Console.WriteLine("3 = Multiplicacion");
+            Console.WriteLine("4 = Division");
+            int x = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
